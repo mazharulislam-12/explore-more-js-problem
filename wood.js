5114d884a74458ba9e45b1643bc3c48eb9f9d88c
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-05 10:08:46 -0700

    wood per pice

---
+++ wood.js
@@ -0,0 +1,22 @@
+/* 
+fixed: per item wood requirements
+1. chair --> 3 cfr
+2. table --> 10 cfr
+3. bed --> 50 cfr
+very: quantity
+
+*/
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity) {
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBeddWood = 50;
+
+    const chairWood = chairQuantity * perChairWood;
+    const tableWood = tableQuantity * perTableWood;
+    const bedWood = bedQuantity * perBeddWood;
+
+    const totalWood = chairWood + tableWood + bedWood;
+    return totalWood;
+}
+const totalWood = woodCalculator(5, 2, 1);
+console.log("total wood required :", totalWood);
