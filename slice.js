1c6708a53f5f1b821d501ac3d98313a6e77a9823
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-05 07:41:42 -0700

    slice and splice

---
+++ slice.js
@@ -0,0 +1,4 @@
+const friends = [24, 66, 34, 63, 12, 56, 44, 33];
+const patrail = friends.slice(0, 3);
+console.log(patrail);
+console.log(friends);
---
+++ splice.js
@@ -0,0 +1,3 @@
+const friends = [24, 66, 34, 63, 12, 56, 44, 33];
+const patrail = friends.splice(0, 3);
+console.log(patrail);
